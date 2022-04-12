ddd40740cc787df250d887215df254e67b22be8f
Simon Druchinin <s.druchinin@stud.sibguti.ru>
2022-04-12 21:41:01 +0700

    Configured a structure of a program

--- Makefile
+++ Makefile
@@ -1,2 +1,29 @@
-all:
-	g++ -Wall -Werror -lm -o main main.cpp
+
+APP_NAME = mygeomprogram
+LIB_NAME = libgeom
+
+BIN_DIR = bin
+SRC_DIR = src
+OBJ_DIR = obj/src
+
+CFLAGS =   -Wall -Werror -I$(SRC_DIR)/$(LIB_NAME)
+APP_PATH = $(BIN_DIR)/$(APP_NAME)
+LIB_PATH = $(OBJ_DIR)/$(LIB_NAME)/$(LIB_NAME).a
+
+
+$(APP_PATH):  $(OBJ_DIR)/$(APP_NAME)/main.o $(OBJ_DIR)/$(LIB_NAME)/libgeom.a
+	g++ $(CFLAGS) $^ -o $@ -lm
+
+$(OBJ_DIR)/$(APP_NAME)/main.o: $(SRC_DIR)/$(APP_NAME)/main.cpp
+	g++ -c $(CFLAGS) $< -o $@ 
+
+$(LIB_PATH): $(OBJ_DIR)/$(LIB_NAME)/geom.o
+	ar rcs  $@ $< 
+
+$(OBJ_DIR)/$(LIB_NAME)/geom.o: $(SRC_DIR)/$(LIB_NAME)/geom.cpp
+	g++ -c $(CFLAGS) $< -o $@ 
+
+.PHONY: clean
+
+clean:
+	rm -rf $(OBJ_DIR)/*/*.o  $(OBJ_DIR)/$(LIB_NAME)/*.a $(BIN_DIR)/$(APP_NAME)
--- src/libgeom/geom.cpp
+++ src/libgeom/geom.cpp
@@ -1,4 +1,5 @@
-#include <iomanip>
+#include <geom.h>
+
 #include <iostream>
 #include <regex>
 #include <string>
@@ -228,8 +229,6 @@ double calc_figure_area(string figure_type, double figure_data[]) {
   return 0;
 }
 
-// #END / BASE() functions;
-
 int get_figure_array_length(string figure_type) {
   if (figure_type == "circle")
     return 3;
@@ -239,30 +238,4 @@ int get_figure_array_length(string figure_type) {
   return 0;
 }
 
-int main(int argc, char **argv) {
-  string figure = " ", figure_type;
-  double f_perimeter, f_area;
-
-  while (true) {
-    cout << "Enter a figure: " << endl;
-    getline(cin, figure);
-    if (figure == "") return 0;
-
-    figure_type = get_figure_type(figure);
-    if (!is_figure_valid(figure, figure_type)) return -1;
-
-    int data_array_len = get_figure_array_length(figure_type);
-    double *figure_data = new double[data_array_len];
-    get_figure_data(figure, figure_type, figure_data);
-
-    f_perimeter = calc_figure_perimeter(figure_type, figure_data);
-    f_area = calc_figure_area(figure_type, figure_data);
-    cout << "perimeter = " << fixed << setprecision(3) << f_perimeter << endl;
-    cout << "area = " << fixed << setprecision(3) << f_area << endl << endl;
-
-    // *Clear memory
-    delete[] figure_data;
-  }
-
-  return 0;
-}
+// #END / BASE() functions;
