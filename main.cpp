091c6a4b5eb1951ce9ce9a27956b0982acf24774
Simon Druchinin <s.druchinin@stud.sibguti.ru>
2022-04-12 19:04:35 +0700

    Added multiple figure addition

--- main.cpp
+++ main.cpp
@@ -240,25 +240,29 @@ int get_figure_array_length(string figure_type) {
 }
 
 int main(int argc, char **argv) {
-  string figure, figure_type;
+  string figure = " ", figure_type;
   double f_perimeter, f_area;
 
-  cout << "Enter a figure: " << endl;
-  getline(cin, figure);
-  figure_type = get_figure_type(figure);
-  if (!is_figure_valid(figure, figure_type)) return -1;
+  while (true) {
+    cout << "Enter a figure: " << endl;
+    getline(cin, figure);
+    if (figure == "") return 0;
 
-  int data_array_len = get_figure_array_length(figure_type);
-  double *figure_data = new double[data_array_len];
-  get_figure_data(figure, figure_type, figure_data);
+    figure_type = get_figure_type(figure);
+    if (!is_figure_valid(figure, figure_type)) return -1;
 
-  f_perimeter = calc_figure_perimeter(figure_type, figure_data);
-  f_area = calc_figure_area(figure_type, figure_data);
-  cout << "perimeter = " << fixed << setprecision(3) << f_perimeter << endl;
-  cout << "area = " << fixed << setprecision(3) << f_area << endl;
+    int data_array_len = get_figure_array_length(figure_type);
+    double *figure_data = new double[data_array_len];
+    get_figure_data(figure, figure_type, figure_data);
 
-  // *Clear memory
-  delete[] figure_data;
+    f_perimeter = calc_figure_perimeter(figure_type, figure_data);
+    f_area = calc_figure_area(figure_type, figure_data);
+    cout << "perimeter = " << fixed << setprecision(3) << f_perimeter << endl;
+    cout << "area = " << fixed << setprecision(3) << f_area << endl << endl;
+
+    // *Clear memory
+    delete[] figure_data;
+  }
 
   return 0;
 }
