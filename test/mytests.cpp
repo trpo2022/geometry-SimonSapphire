b86bca24c218ee440fdb609093ac012ccdc3ed18
Simon Druchinin <s.druchinin@stud.sibguti.ru>
2022-05-14 15:08:50 +0700

    is_segments_intersected() test

--- test/mytests.cpp
+++ test/mytests.cpp
@@ -17,3 +17,80 @@ CTEST(suite1, isinto) {
   ASSERT_EQUAL(0, isInto(2.2, 1.2, 2.2));
   ASSERT_EQUAL(1, isInto(1.21, 1.2, 2.2));
 }
+
+CTEST(suite1, segments) {
+  CTEST_LOG("checking is_segments_intersected() function..");
+
+  double p1_x, p1_y, p2_x, p2_y, triangle_data[4];
+  p1_x = -1;
+  p1_y = 1;
+  p2_x = 1;
+  p2_y = -1;
+  triangle_data[0] = -1;
+  triangle_data[1] = -1;
+  triangle_data[2] = 1;
+  triangle_data[3] = 1;
+
+  ASSERT_EQUAL(1, is_segments_intersected(p1_x, p1_y, p2_x, p2_y,
+                                          triangle_data[0], triangle_data[1],
+                                          triangle_data[2], triangle_data[3]));
+
+  p1_x = 1;
+  p1_y = -1;
+  p2_x = 1;
+  p2_y = 1;
+
+  ASSERT_EQUAL(0, is_segments_intersected(p1_x, p1_y, p2_x, p2_y,
+                                          triangle_data[0], triangle_data[1],
+                                          triangle_data[2], triangle_data[3]));
+
+  p1_x = 1;
+  p1_y = -1;
+  p2_x = 0;
+  p2_y = 0;
+
+  p1_x = 1;
+  p1_y = -1;
+  p2_x = 1;
+  p2_y = 1;
+
+  ASSERT_EQUAL(0, is_segments_intersected(p1_x, p1_y, p2_x, p2_y,
+                                          triangle_data[0], triangle_data[1],
+                                          triangle_data[2], triangle_data[3]));
+
+  p1_x = 1;
+  p1_y = -1;
+  p2_x = 0;
+  p2_y = 0;
+
+  ASSERT_EQUAL(0, is_segments_intersected(p1_x, p1_y, p2_x, p2_y,
+                                          triangle_data[0], triangle_data[1],
+                                          triangle_data[2], triangle_data[3]));
+
+  p1_x = 1;
+  p1_y = -1;
+  p2_x = 0;
+  p2_y = 0.01;
+
+  ASSERT_EQUAL(1, is_segments_intersected(p1_x, p1_y, p2_x, p2_y,
+                                          triangle_data[0], triangle_data[1],
+                                          triangle_data[2], triangle_data[3]));
+
+  p1_x = -1;
+  p1_y = 1;
+  p2_x = -1;
+  p2_y = -1;
+
+  ASSERT_EQUAL(0, is_segments_intersected(p1_x, p1_y, p2_x, p2_y,
+                                          triangle_data[0], triangle_data[1],
+                                          triangle_data[2], triangle_data[3]));
+
+  p1_x = 1;
+  p1_y = -1;
+  p2_x = 0;
+  p2_y = 0;
+
+  ASSERT_EQUAL(0, is_segments_intersected(p1_x, p1_y, p2_x, p2_y,
+                                          triangle_data[0], triangle_data[1],
+                                          triangle_data[2], triangle_data[3]));
+}
